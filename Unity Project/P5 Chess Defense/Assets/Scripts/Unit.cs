ad3d599b1443e0761af2b51885a0560903c16c8f
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-09-07 13:38:00 +0200

    Base classes
    Classes: Unit, Tower en Enemy have been made.

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Unit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+    public int health;
+    public int damage;
+
+	// Use this for initialization
+	void Start () {
+        SetValues();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void Move ()
+    {
+        //movement code goes here
+    }
+
+    public virtual void SetValues ()
+    {
+        health = 1;
+        damage = 1;
+    }
+
+    public virtual void Attack ()
+    {
+        //attack code here
+    }
+
+    public virtual void DoDamage ()
+    {
+        //damage code here
+    }
+}
