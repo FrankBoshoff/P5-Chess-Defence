e3bc34fc4d1b334b93f0ecd4259485e2e323892f
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-09-11 17:11:36 +0200

    bezig met navemesh
    dd

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBase : MonoBehaviour
+{
+    Transform navemeshTarget;
+    NavMeshAgent enemyModel;
+
+    public int enmyMoveSpeed;
+    public int enemyHeath;
+
+    void Start()
+    {
+        navemeshTarget = GameObject.FindWithTag("Wizard").transform;
+        EnemyMove();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public virtual void EnemyMove()
+    {
+        enemyModel = this.GetComponent<NavMeshAgent>();
+        enemyModel.destination = navemeshTarget.position;
+
+    }
+}
