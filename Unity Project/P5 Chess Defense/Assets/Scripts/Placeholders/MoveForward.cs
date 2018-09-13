c4d2da0947c06e0b312ba5e1cecccb2b9fad7f34
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-09-13 10:02:29 +0200

    Midwork Push
    Basic tower idea is semi-functional.

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Placeholders/MoveForward.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+
+    public Vector3 v;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position += (v * Time.deltaTime);
+	}
+}
---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    public int range;
+    public float fireTimer;
+    public int damage;
+    public bool targeting;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        fireTimer += Time.deltaTime;
+		if(targeting == true && fireTimer >= 0.5)
+        {
+            Fire();
+        }
+	}
+
+    public void Fire()
+    {
+        print("damage");
+        fireTimer = 0;
+    }
+
+    private void OnTriggerStay(Collider collision)
+    {
+        if (targeting == false)
+        {
+            target = collision.transform;
+            targeting = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        targeting = false;
+    }
+}
