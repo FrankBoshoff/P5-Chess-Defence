728d6c04bf8d14e6be6288a6f41eb27b04654daf
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-09-13 11:43:40 +0200

    ff
    ff

--- Unity Project/P5 Chess Defense/Assets/Scripts/WizardScript.cs
+++ Unity Project/P5 Chess Defense/Assets/Scripts/WizardScript.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class WizardScript : MonoBehaviour
 {
     public int wizardHealth;
+    public Vector3 wizardPos;
+
+    void OnCollisionExit(Collision collision)
+    {
+        this.gameObject.transform.position = wizardPos;
+    }
 }
