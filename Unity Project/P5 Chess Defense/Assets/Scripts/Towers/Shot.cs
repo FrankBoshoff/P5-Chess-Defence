9a0c6c9f1744899b600473da52d15818c9fd8d03
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-09-14 15:50:52 +0200

    Base Tower target list
    most basic tower half works

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Towers/Shot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+    public int damage;
+    public int speed;
+    public bool poisonous;
+    public bool explosive;
+    public Transform target;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Towers/TowerBase.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour {
+
+    public int cost;
+    public int range;
+    public float timer;
+    public List<Transform> targets = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<SphereCollider>().radius = range;
+        timer = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public virtual void Ability()
+    {
+        //tower attack/effect goes here
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        //add enemy to target list
+        targets.Add(collision.transform);
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        //remove enemy from target list
+        targets.Remove(collision.transform);
+    }
+}
