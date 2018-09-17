e2180ebd29b51aa18042f9c73088e70832ec170f
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-09-17 11:13:32 +0200

    base spawener
    17-9

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> enemyHolder;
+    public float spawntime;
+    public bool maySpawn;
+    GameObject rEnemy;
+
+    void Start()
+    {
+        maySpawn = true;
+    }
+
+    void Update()
+    {
+        if (maySpawn == true)
+        {
+            maySpawn = false;
+            RendomEnemy();
+        }
+    }
+
+    public void RendomEnemy()
+    {
+        rEnemy = enemyHolder[Random.Range(0, enemyHolder.Count)];
+        StartCoroutine(SpawnEnemy());
+    }
+
+    public IEnumerator SpawnEnemy()
+    {
+        yield return new WaitForSeconds(spawntime);
+        Instantiate(rEnemy, transform.position, Quaternion.identity);
+        maySpawn = true;
+    }
+}
