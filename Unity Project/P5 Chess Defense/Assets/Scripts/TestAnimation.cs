562c2838a8378e4f6601feab700f7b3837b3c57c
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2018-09-19 14:45:25 +0200

    kopie
    ss

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/TestAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimation : MonoBehaviour
+{
+    //Dit script is tijdelijk om te testen als alles werkt gooi ik hem weg //Jorrit
+    public GameObject projectal;
+    public ParticleSystem partical;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            projectal.SetActive(true);
+        }
+        if (partical.IsAlive() == false)
+        {
+            projectal.SetActive(false);
+        }
+    }
+
+}
