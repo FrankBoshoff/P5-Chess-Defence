494788afbf06ed4f6dd4a60ef433800aeb79ded1
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-09-20 11:36:32 +0200

    Mid work push
    Basic main menu is there

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button start;
+    public Button options;
+    public Button exit;
+
+	// Use this for initialization
+	void Start () {
+        start.onClick.AddListener(StartGame);
+        options.onClick.AddListener(Options);
+        exit.onClick.AddListener(ExitGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        print("start game");
+    }
+
+    public void Options()
+    {
+        print("options");
+    }
+
+    public void ExitGame()
+    {
+        print("exit");
+    }
+}
