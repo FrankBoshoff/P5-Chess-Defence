4499dbd20bdcc5bfe1c699a8a94436efeec5e4db
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-10-29 10:42:54 +0100

    Info Panels for shop
    Shop now displays info panels when hovering over a button.

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/ShopPopup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopPopup : MonoBehaviour {
+
+    public GameObject g;
+    //public GameObject manager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DisplayInfo()
+    {
+        if (g.activeSelf == false)
+        {
+            //manager.GetComponent<UIManager>().shop = gameObject;
+            g.transform.position = Input.mousePosition;
+            print("temp");
+            g.SetActive(true);
+        }
+    }
+
+    public void RemoveInfo()
+    {
+        if (g.activeSelf == true)
+        {
+            g.SetActive(false);
+        }
+    }
+}
