6b808183870c09b3138f0c644c5a264a8a988802
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-11-05 09:16:07 +0100

    Light push
    Lights now have flicker

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+    private Light l;
+    public float min;
+    public float max;
+    private float timer;
+    public float time;
+
+	// Use this for initialization
+	void Start () {
+        l = gameObject.GetComponent<Light>();
+        timer = time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            l.intensity = Random.Range(min, max);
+            timer = time;
+        }
+	}
+}
