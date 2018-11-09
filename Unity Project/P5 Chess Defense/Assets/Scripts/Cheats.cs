135708b37128650ffb4b21d1236049f04e6e1694
Daniel-Stremmelaar <33859025+Daniel-Stremmelaar@users.noreply.github.com>
2018-11-09 14:30:10 +0100

    Cheat push
    Cheat script ready

---
+++ Unity Project/P5 Chess Defense/Assets/Scripts/Cheats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : MonoBehaviour {
+
+    private GameObject manager;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.FindGameObjectWithTag("UImanager");		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("Comma") && Time.timeScale != 0)
+        {
+            Time.timeScale = 0.5f;
+        }
+        if (Input.GetKeyDown("Period") && Time.timeScale != 0)
+        {
+            Time.timeScale = 2;
+        }
+        if (Input.GetKeyDown("Slash") && Time.timeScale != 0)
+        {
+            Time.timeScale = 1;
+        }
+        if (Input.GetKeyDown("Semicolon") && Time.timeScale != 0)
+        {
+            manager.GetComponent<UIManager>().UpdateEconomyUI(5);
+        }
+    }
+}
